2cba32eced9a4933e629404505c1eb07fd72dddd
denwell76 <danielnieminen04@gmail.com>
2025-03-31 00:08:25 +0300

    vite.config reitti muokattu

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Carlist from "./components/carlist"
 import Container from '@mui/material/Container';
-import CssBaseLine from '@mui/material/CssBaseLine';
+import CssBaseLine from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
 import  Toolbar  from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
